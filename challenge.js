ef48f898a2d315ec8532a2f4d8125f78bdeef425
zaki tayyip <zakidayib1997@gmail.com>
2023-05-20 11:31:54 +0300

    solved challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let num1 = 10;
+let num2 = 12;
+
+if (num1>num2){
+    console.log(`${num1} is greater than ${num2}`);
+}
+
+else if (num2>num1){
+    console.log(`${num2} is greater than ${num1}`)
+}
